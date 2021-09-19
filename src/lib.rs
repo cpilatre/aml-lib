be16c7c83a479d071026881bfe53d54a9b620442
Claude Pilatre <claude.pilatre@gmail.com>
2021-09-19 18:16:45 +0200

    Improve AmlError

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,19 @@ pub enum AmlError {
     /// You have tried to parse an corrumpted base64 SMS data.
     InvalidBase64,
 }
+
+impl std::error::Error for AmlError {}
+
+impl std::fmt::Display for AmlError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let text = match *self {
+            AmlError::UnimplementedVersion => {
+                String::from("You have tried to parse an unimplemented version of SMS AML")
+            }
+            AmlError::InvalidBase64 => {
+                String::from("You have tried to parse an corrumpted base64 SMS data")
+            }
+        };
+        write!(f, "Error: {}", text)
+    }
+}
